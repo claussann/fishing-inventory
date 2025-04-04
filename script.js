174801cda9271fb2ab229ecc648b2e502b98b102
Claudio Sanna <ccc@test.it>
2025-04-04 23:49:44 +0200

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const minuteria = document.getElementById("insMinuteria").value
+const mulinelli = document.getElementById("insMulinelli").value
+const canneDaPesca = document.getElementById("insCanneDaPesca").value
+const filo = document.getElementById("insFilo").value
+const esche = document.getElementById("insEsche").value
+
+async function sendProducts(){
+const inventory = await fetch('')
+}
