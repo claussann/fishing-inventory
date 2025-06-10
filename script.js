23bf2a8f6fa82a17ff7c24711c9c2e085882359e
claussann <clausann92@gmail.com>
2025-06-10 19:01:26 +0200

    feat: implement button for save product in local storage and render product

--- script.js
+++ script.js
@@ -1,9 +1,104 @@
-const minuteria = document.getElementById("insMinuteria").value
-const mulinelli = document.getElementById("insMulinelli").value
-const canneDaPesca = document.getElementById("insCanneDaPesca").value
-const filo = document.getElementById("insFilo").value
-const esche = document.getElementById("insEsche").value
 
-async function sendProducts(){
-const inventory = await fetch('')
+const minut = []
+const muli = []
+const canne = []
+const fili = []
+const esc = []
+
+const allProduct = [{
+    minuteria: minut,
+    mulinelli: muli,
+    canneDaPesca: canne,
+    filo: fili,
+    esche: esc
+}]
+
+function loadProducts() {
+    if (localStorage.getItem("minuteria")) {
+        minut.push(...JSON.parse(localStorage.getItem("minuteria")))
+    }
+    if (localStorage.getItem("mulinelli")) {
+        muli.push(...JSON.parse(localStorage.getItem("mulinelli")))
+    }
+    if (localStorage.getItem("canneDaPesca")) {
+        canne.push(...JSON.parse(localStorage.getItem("canneDaPesca")))
+    }
+    if (localStorage.getItem("filo")) {
+        fili.push(...JSON.parse(localStorage.getItem("filo")))
+    }
+    if (localStorage.getItem("esche")) {
+        esc.push(...JSON.parse(localStorage.getItem("esche")))
+    }
+}
+
+window.onload = loadProducts
+
+function sandProducts() {
+    const elMinuteria = document.getElementById("insMinuteria")
+    const elMulinelli = document.getElementById("insMulinelli")
+    const elCanneDaPesca = document.getElementById("insCanneDaPesca")
+    const elFilo = document.getElementById("insFilo")
+    const elEsche = document.getElementById("insEsche")
+    const minuteria = elMinuteria.value
+    const mulinelli = elMulinelli.value
+    const canneDaPesca = elCanneDaPesca.value
+    const filo = elFilo.value
+    const esche = elEsche.value
+
+    if (minuteria) {
+        minut.push(minuteria)
+        elMinuteria.value = ""
+        localStorage.setItem("minuteria", JSON.stringify(minut))
+    }
+    if (mulinelli) {
+        muli.push(mulinelli)
+        elMulinelli.value = ""
+        localStorage.setItem("mulinelli", JSON.stringify(muli))
+    }
+    if (canneDaPesca) {
+        canne.push(canneDaPesca)
+        elCanneDaPesca.value = ""
+        localStorage.setItem("canneDaPesca", JSON.stringify(canne))
+    }
+    if (filo) {
+        fili.push(filo)
+        elFilo.value = ""
+        localStorage.setItem("filo", JSON.stringify(fili))
+    }
+    if (esche) {
+        esc.push(esche)
+        elEsche.value = ""
+        localStorage.setItem("esche", JSON.stringify(esc))
+    }
+}
+
+function showMinuteria() {
+    document.getElementById("prodotti").innerHTML = ``
+    document.getElementById("prodotti").innerHTML = minut.map((minut) => `<li>${minut}</li>`).join("")
+}
+function showMulinelli() {
+    document.getElementById("prodotti").innerHTML = ``
+    document.getElementById("prodotti").innerHTML = muli.map((muli) => `<li>${muli}</li>`).join("")
+}
+function showCanneDaPesca() {
+    document.getElementById("prodotti").innerHTML = ``
+    document.getElementById("prodotti").innerHTML = canne.map((canne) => `<li>${canne}</li>`).join("")
+}
+function showFilo() {
+    document.getElementById("prodotti").innerHTML = ``
+    document.getElementById("prodotti").innerHTML = fili.map((filo) => `<li>${filo}</li>`).join("")
+}
+function showEsche() {
+    document.getElementById("prodotti").innerHTML = ``
+    document.getElementById("prodotti").innerHTML = esc.map((esche) => `<li>${esche}</li>`).join("")
+}
+
+function showAllProducts() {
+    document.getElementById("prodotti").innerHTML = ``
+    document.getElementById("prodotti").innerHTML = allProduct.map((prod) => `
+    <h6>Minuteria: ${prod.minuteria}</h6>
+    <h6>Mulinelli: ${prod.mulinelli}</h6>
+    <h6>Canne da Pesca: ${prod.canneDaPesca}</h6>
+    <h6>Filo: ${prod.filo}</h6><h6>Esche: ${prod.esche}</h6>
+    `).join("")
 }
